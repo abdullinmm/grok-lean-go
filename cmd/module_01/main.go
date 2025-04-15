2558d1c3d2492dcbfdcb4a152c3866b0ef131241
Marsel Abdullin <abdullin.marsel@otr.ru>
2025-04-15 19:20:32 +0300

    Added a program that outputs a message to the console like this: Hello, [name]! Your grades: [grades]. Average grade: [average].

--- cmd/module_01/main.go
+++ cmd/module_01/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	name = "Alice"
+	s    = []int{2, 4, 7}
+)
+
+func main() {
+	fmt.Printf("Привет, %s! Твои оценки: %v. Средняя оценка: %.2f.", name, s, middelNumber(s))
+}
+
+func middelNumber(s []int) float64 {
+	var num float64
+	for _, v := range s {
+		num += float64(v)
+	}
+	return num / float64(len(s))
 }
