a00ae7dbe55b8008b74dbcc02aac587b0d80f249
Marsel Abdullin <abdullin.marsel@otr.ru>
2025-04-15 21:10:50 +0300

    Added isEven function that checks if a number is even and outputs the result to the console

--- cmd/module_01/main.go
+++ cmd/module_01/main.go
@@ -5,10 +5,12 @@ import "fmt"
 var (
 	name = "Alice"
 	s    = []int{2, 4, 7}
+	num  = 4
 )
 
 func main() {
 	fmt.Printf("Привет, %s! Твои оценки: %v. Средняя оценка: %.2f.", name, s, middelNumber(s))
+	fmt.Printf("\nЧисло %d является четным: %t", num, IsEven(num))
 }
 
 func middelNumber(s []int) float64 {
@@ -17,4 +19,8 @@ func middelNumber(s []int) float64 {
 		num += float64(v)
 	}
 	return num / float64(len(s))
+}
+
+func IsEven(num int) bool {
+	return num%2 == 0
 }
