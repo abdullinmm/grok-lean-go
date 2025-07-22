c0f169c88c493c802c3eea2e7d64e501242ea6db
Marsel Abdullin <abdullin.marsel@otr.ru>
2025-07-22 14:49:34 +0300

    fixed build bug

--- internal/practic/anagrams.go
+++ internal/practic/anagrams.go
@@ -1,18 +1,16 @@
 package practic
 
-import "fmt"
-
 func Anagrams(s1 string, s2 string) error {
-	if len(s1) != len(s2) || len(s1) < 1 || len(s2) < 1 {
-		return fmt.Errorf("string not anagram")
-	}
+	// if len(s1) != len(s2) || len(s1) < 1 || len(s2) < 1 {
+	// 	return fmt.Errorf("string not anagram")
+	// }
 
-	r1 := []rune(s1)
-	r2 := []rune(s2)
+	// r1 := []rune(s1)
+	// r := []rune(s2)
 
-	for _, v1 := range r1 {
-		// TODO: logic here
-	}
+	// for _, v1 := range r1 {
+	// 	// TODO: logic here
+	// }
 
 	return nil
 }
